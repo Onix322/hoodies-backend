ba9c72661f684d61ecf8a3bb5caa199691a07582
Alex <alexandru.dobos3@gmail.com>
2025-03-25 16:24:03 +0100

    added url

--- src/main/java/com/hoodiesbackend/config/WebMvcConfig.java
+++ src/main/java/com/hoodiesbackend/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://hoodies-frontend.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
